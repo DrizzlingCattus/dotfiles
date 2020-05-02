c905d8ebae62c94eee31c0f0bec99cfbab8d904b
DrizzlingCattus <hit0473@gmail.com>
2020-05-02 20:40:09 +0900

    Feat: vim 설치 스크립트 추가

---
+++ install-vim.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# vim-plug for nvim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# not yet
