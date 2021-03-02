b3175ce7cb2b82b2d45d7cd33d8ecb6e15fd25a0
DrizzlingCattus <hit0473@gmail.com>
2021-03-03 02:38:54 +0900

    Add: snippet directory

--- install-vim.sh
+++ install-vim.sh
@@ -13,12 +13,14 @@ curl --insecure -fLo ~/.vim/autoload/plug.vim https://raw.github.com/junegunn/vi
 
 # vimrc
 mv -v ~/.vimrc ~/.vimrc.old 2> /dev/null
-ln -sf $BASE/vimrc ~/.vimrc
+ln -sfv $BASE/vimrc ~/.vimrc
+mv -v ~/.vim/snippet ~/.vim/snippet.old 2> /dev/null
+ln -sfv $BASE/snippet ~/.vim
 
 # nvim
 mkdir -p ~/.config/nvim/autoload
-ln -sf $BASE/nvim/init.vim ~/.config/nvim/init.vim
-ln -sf ~/.vim/autoload/plug.vim ~/.config/nvim/autoload/
-ln -sf $BASE/coc-settings.json ~/.config/nvim/coc-settings.json
+ln -sfv $BASE/nvim/init.vim ~/.config/nvim/init.vim
+ln -sfv ~/.vim/autoload/plug.vim ~/.config/nvim/autoload/
+ln -sfv $BASE/coc-settings.json ~/.config/nvim/coc-settings.json
 
 vim +PlugInstall +qall
